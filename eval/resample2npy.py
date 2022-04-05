97d705f9ee21bd2d99844990230b10f9aca3c6d7
klean2050 <k.avramidis@windowslive.com>
2022-04-04 17:25:12 -0700

    extend to wavs and arbitrary fs

--- eval/resample2npy.py
+++ eval/resample2npy.py
@@ -1,39 +1,32 @@
 '''
-This script takes a directory of mp3s as input, and for each mp3, resamples to 
-the provided sampling rate and outputs in numpy format. To be used for downstream tasks.
+This script takes a directory of mp3 files as input, and resamples each mp3 file
+to the provided sampling rate and numpy format. To be used for downstream tasks.
 '''
 
-import os
-import numpy as np
-import glob
-import librosa
-import fire
-import tqdm
-import sys
-
+import os, glob, fire, tqdm
+import numpy as np, librosa
 
 class Processor:
-	def __init__(self):
-		self.fs = 16000 # Sampling rate for resampling
+	def __init__(self, fs=16000, format="mp3"):
+		self.fs = fs # Sampling rate for resampling
+		self.format = format # Typically mp3 or wav
 
-    # Find all mp3s contained in the given directory
+    # Find all files contained in the given directory
 	def get_paths(self, data_path, output_path):
-		self.files = glob.glob(os.path.join(data_path, '**/*.mp3'), recursive=True)
+		self.files = glob.glob(os.path.join(data_path, f'**/*.{format}'), recursive=True)
 		self.npy_path = os.path.join(output_path, 'npy')
-
-		if not os.path.exists(output_path):
-			os.makedirs(output_path)
+		os.makedirs(output_path, exist_ok=True)
     
-    # Resample
+    # Resample a file
 	def get_npy(self, fn):
-		x, sr = librosa.core.load(fn, sr=self.fs)
+		x, _ = librosa.core.load(fn, sr=self.fs)
 		return x
 
 	def iterate(self, data_path):
-		output_path = './npy_data' # Replace with desired output directory
+		output_path = './npy_audios' # Replace with desired output directory
 		self.get_paths(data_path, output_path)
 		for fn in tqdm.tqdm(self.files):
-			output_fn = os.path.join(output_path, fn.split('/')[-1][:-3]+'npy')
+			output_fn = os.path.join(output_path, fn.replace(f".{format}", ".npy"))
 			if not os.path.exists(output_fn):
 				try:
 					x = self.get_npy(fn)
@@ -44,5 +37,6 @@ class Processor:
 					continue
 
 if __name__ == '__main__':
-	p = Processor()
+
+	p = Processor(fs=16000, format="wav")
 	fire.Fire({'run': p.iterate})
