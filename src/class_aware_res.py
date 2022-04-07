054aef66d9061a0987991d73792ce0cdfc926741
klean2050 <k.avramidis@windowslive.com>
2022-04-07 01:50:43 -0700

    not sure about this class

--- src/class_aware_res.py
+++ src/class_aware_res.py
@@ -2,12 +2,10 @@
 Below is an implementation of class-aware resampling,
 modified from Tu et al. (https://arxiv.org/abs/2007.09654).
 """
-
 # coding: utf-8
-import numpy as np
-import torch
-import random
+import numpy as np, random, torch
 from torch.utils.data import Sampler
+
 from utils import TAGS
 
 
@@ -31,21 +29,15 @@ class RandomCycleIter:
 
 
 def class_aware_sample_generator(cls_iter, data_iter_list, n, num_samples_cls=1):
-    i = 0
-    j = 0
+    i, j = 0, 0
     while i < n:
-
         # yield next(data_iter_list[next(cls_iter)])
-
-        if j >= num_samples_cls:
-            j = 0
+        j = 0 if j >= num_samples_cls else j
         if j == 0:
-
             temp_tuple = next(zip(*[data_iter_list[next(cls_iter)]] * num_samples_cls))
             yield temp_tuple[j]
         else:
             yield temp_tuple[j]
-
         i += 1
         j += 1
 
@@ -54,24 +46,19 @@ class ClassAwareSampler(Sampler):
     def __init__(self, data_source, num_samples_cls=3, reduce=4):
         random.seed(0)
         torch.manual_seed(0)
-        num_classes = len(TAGS)
+        self.epoch = 0
+        self.num_classes = len(TAGS)
         self.class_counts = data_source.get_class_weights()
 
-        self.epoch = 0
-
-        self.class_iter = RandomCycleIter(range(num_classes))
-
+        self.class_iter = RandomCycleIter(range(self.num_classes))
         self.cls_data_list = data_source.build_cls_data_list()
-
-        self.num_classes = len(TAGS)
-        self.data_iter_list = [
-            RandomCycleIter(x) for x in self.cls_data_list
-        ]  # repeated
+        self.data_iter_list = [RandomCycleIter(x) for x in self.cls_data_list]  # repeated
         self.num_samples = int(max(self.class_counts) * self.num_classes / reduce)
         self.num_samples_cls = num_samples_cls
+
         print(
-            ">>> Class Aware Sampler Built! Class number: {}, reduce {}".format(
-                num_classes, reduce
+            "Class-aware Sampler Built! Class number: {}, reduce {}".format(
+                self.num_classes, reduce
             )
         )
 
@@ -87,13 +74,10 @@ class ClassAwareSampler(Sampler):
         self.epoch = epoch
 
     def get_sample_per_class(self):
-        condition_prob = np.zeros([self.num_classes, self.num_classes])
         sample_per_cls = np.asarray([len(x) for x in self.gt_labels])
-        rank_idx = np.argsort(-sample_per_cls)
-
-        for i, cls_labels in enumerate(self.gt_labels):
-            num = len(cls_labels)
-            condition_prob[i] = np.sum(np.asarray(cls_labels), axis=0) / num
-
+        condition_prob = [
+            np.sum(np.asarray(cls_labels), axis=0) / len(cls_labels)
+            for cls_labels in self.gt_labels
+        ]
         sum_prob = np.sum(condition_prob, axis=0)
         need_sample = sample_per_cls / sum_prob
