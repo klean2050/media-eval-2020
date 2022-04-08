3ca2a21e067972a5711a80eab48444b7cd9d7802
klean2050 <k.avramidis@windowslive.com>
2022-04-07 17:47:59 -0700

    revamp resampling process

--- src/resample2npy.py
+++ src/resample2npy.py
@@ -3,31 +3,8 @@ This script takes a directory of mp3 files as input, and resamples each mp3 file
 to the provided sampling rate and numpy format. To be used for downstream tasks.
 """
 
-import os, fire, tqdm
-import torchaudio, torch
-import numpy as np, torch.nn as nn
-
-
-class ChangeSampleRate(nn.Module):
-    """
-    https://discuss.pytorch.org/t/change-sample-rate-using-torchaudio/23177/7
-    """
-
-    def __init__(self, input_rate: int, output_rate: int):
-        super().__init__()
-        self.output_rate = output_rate
-        self.input_rate = input_rate
-
-    def forward(self, wav: torch.tensor) -> torch.tensor:
-        # Only accepts 1-channel waveform input
-        wav = wav.view(wav.size(0), -1)
-        new_length = wav.size(-1) * self.output_rate // self.input_rate
-        indices = torch.arange(new_length) * (self.input_rate / self.output_rate)
-        round_down = wav[:, indices.long()]
-        round_up = wav[:, (indices.long() + 1).clamp(max=wav.size(-1) - 1)]
-        return round_down * (1.0 - indices.fmod(1.0)).unsqueeze(
-            0
-        ) + round_up * indices.fmod(1.0).unsqueeze(0)
+import os, tqdm, librosa
+import argparse, numpy as np
 
 
 class Processor:
@@ -35,35 +12,25 @@ class Processor:
         self.fs = fs  # Sampling rate for resampling
         self.format = format  # Typically mp3 or wav
 
-    def get_paths(self, data_path, output_path):
-        # Find all files contained in the given directory
-        self.files = [data_path + f for f in os.listdir(data_path)]
-        # os.path.join(data_path, f"**.{format}"))#recursive=True)
-        self.npy_path = os.path.join(output_path, "npy")
+    def iterate(self, data_path, output_path="../data/npy_audios/"):
         os.makedirs(output_path, exist_ok=True)
-
-    def get_npy(self, fn):
-        # Resample a file
-        x, sr = torchaudio.load(fn)
-        x = x.sum(axis=0) / 2
-        csr = ChangeSampleRate(sr, self.fs)
-        out_wav = csr(x.unsqueeze(0))
-        return out_wav.numpy()
-
-    def iterate(self, data_path):
-        output_path = "../data/npy_audios/"  # Replace with desired output directory
-        self.get_paths(data_path, output_path)
+        self.files = [data_path + f for f in os.listdir(data_path)]
         for fn in tqdm.tqdm(self.files):
-            output_fn = os.path.join(output_path, fn.replace(f".{format}", ".npy"))
+            name = fn.split("/")[-1]
+            output_fn = os.path.join(output_path, name.replace(f".{self.format}", ".npy"))
             try:
-                x = self.get_npy(fn)
-                np.save(open(output_fn, "wb"), x)
+                x, _ = librosa.core.load(fn, sr=self.fs)
+                np.save(output_fn, x)
             except RuntimeError:
-                # some audio files are broken
+                # some audio files may be broken
                 continue
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--data_path", type=str, default="../data")
+    parser.add_argument("--output_path", type=str, default="../data/npy_audios")
+    config = parser.parse_args()
 
     p = Processor(fs=16000, format="wav")
-    fire.Fire({"run": p.iterate})
+    p.iterate(config.data_path, config.output_path)
